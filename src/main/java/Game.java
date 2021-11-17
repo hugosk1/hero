54a8e26fc03652db94fd53a22db6a1fa75947118
hugosk1 <hugoml76@hotmail.com>
2021-11-17 17:27:32 +0000

    Reading keystrokes

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,5 @@
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -34,5 +36,28 @@ public class Game {
         screen.clear();
         arena.draw(screen.newTextGraphics());
         screen.refresh();
+    }
+
+
+    private void processKey(KeyStroke key) {
+        arena.processKey(key);
+    }
+
+
+    public void run() throws IOException {
+        while (true) {
+            draw();
+            KeyStroke key = screen.readInput();
+
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                try {
+                    screen.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (key.getKeyType() == KeyType.EOF) break;
+            else processKey(key);
+        }
     }
 }
