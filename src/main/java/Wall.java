d4b99bca00823d9b8a53e012acf0558029a5fbfc
hugosk1 <hugoml76@hotmail.com>
2021-11-17 22:30:36 +0000

    Walls

--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 
-public class Wall {
+public class Wall extends Element {
+
+    public Wall(int x, int y) {
+        super(x, y);
+    }
+
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#d3d3d3"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(getPosition().getX(), getPosition().getY()), "#");
+    }
 }
